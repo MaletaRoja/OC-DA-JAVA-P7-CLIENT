b61ae6177e40e6cf380ab5aab8970ef85ec46d02
Senpothia <kintelalpz@gmail.com>
2020-05-03 16:48:47 +0200

    Modifications apllication.propertie, template index

--- OC-DA-JAVA-P7-client/src/main/java/com/formation/projet7/OcDaJavaP7Application.java
+++ OC-DA-JAVA-P7-client/src/main/java/com/formation/projet7/OcDaJavaP7Application.java
@@ -2,11 +2,13 @@ package com.formation.projet7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.formation.projet7")
-public class OcDaJavaP7Application {
+
+public class OcDaJavaP7Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcDaJavaP7Application.class, args);
